6fc8b87bfc91f0c165141df9ee9f6256384e7cac
Shab09 <shabbir52murtaza@gmail.com>
2019-11-17 16:31:39 +0500

    Resize and improved functionality

--- Assets/_Game/BlockSystem/Generator/BlockSystemGenerator.cs
+++ Assets/_Game/BlockSystem/Generator/BlockSystemGenerator.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 
 public class BlockSystemGenerator : MonoBehaviour
 {   
-    public static BlockSystemGenerator blockSystemGenerator;
-    public GameObject generator;
+    public static BlockSystemGenerator blockSystemGenerator;// Globel Reference to the system
+    public GameObject generator;//Object to generate
+    public Vector3 generationPosition = new Vector3(0,21,0);
     
     private void Start() {
+        //Make sytem accessable
         blockSystemGenerator=this;
     }
     public void Genrate(){
-        GameObject temp=Instantiate(generator);
-        temp.transform.position = transform.position;
+        GameObject temp=Instantiate(generator); // Create new gameobject
+        temp.GetComponent<GenerateBlocks>().setnewPosition(generationPosition);// set objects position
     }
 }
--- Assets/_Game/BlockSystem/Scripts/GenerateBlocks.cs
+++ Assets/_Game/BlockSystem/Scripts/GenerateBlocks.cs
@@ -2,9 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GenerateBlocks : MonoBehaviour
-{
-    [Header("Block System")]
+public class GenerateBlocks : MonoBehaviour {
+    [Header ("Block System")]
     public List<GameObject> objToGenerate;
     public float xInit = -14;
     public float xEnd = 14;
@@ -13,28 +12,31 @@ public class GenerateBlocks : MonoBehaviour
     private Vector3 newPositon;
 
     // Start is called before the first frame update
-    void Awake()
-    {
-        newPositon = transform.position;
-        float xValue= xInit;
-        while(xValue<=xEnd){
-            GameObject obj = Instantiate(objToGenerate[Random.Range(0,objToGenerate.Count-1)]);
-            obj.transform.parent=transform;
-            obj.transform.localPosition = new Vector3(xValue,0,0);
-            xValue+=xSpan;
+    void Awake () {
+        newPositon=transform.position;
+        float xValue = xInit;
+        while (xValue <= xEnd) {
+            if (Random.Range (0, 100) > 50) {
+                GameObject obj = Instantiate (objToGenerate[Random.Range (0, objToGenerate.Count - 1)]);
+                obj.transform.parent = transform;
+                obj.transform.localPosition = new Vector3 (xValue, 0, 0);
+            }
+            xValue += xSpan;
         }
     }
-
     // Update is called once per frame
-    void Update()
-    {
-        if(newPositon!=transform.position){
+    void Update () {
+        if (newPositon != transform.position) {
             //Move towards the position to move
             transform.position =
                 Vector3.MoveTowards (transform.position, newPositon, 100 * Time.deltaTime);
         }
     }
-    public void moveDown(){
-        newPositon.y -=ySpen;
+    public void moveDown () {
+        newPositon.y -= ySpen;
+    }
+
+    public void setnewPosition(Vector3 NPositon){
+        newPositon = NPositon;
     }
 }
